6c315f6b625e719dad03de22e8cf625d87639716
Marko Harjula <marko.harjula@gmail.com>
2019-10-24 08:01:56 +0300

    update testing and packages

--- test/testHetu.ts
+++ test/testHetu.ts
@@ -1,18 +1,18 @@
 process.env.NODE_ENV = 'test';
 import {expect} from 'chai';
-import {describe, it} from 'mocha';
+import 'mocha';
 import {isFemale, isMale, isValidPersonId} from '../src/';
 
-describe('test channels', () => {
-	it('validate hetu', () => {
+describe('test hetu', () => {
+	it('should validate hetu', () => {
 		expect(isValidPersonId('131052-308T')).to.be.eq(true);
 		expect(isValidPersonId('131052-3082')).to.be.eq(false);
 	});
-	it('should fail if too long or short', () => {
+	it('should fail if too long or short hetu', () => {
 		expect(isValidPersonId('131052-308TA')).to.be.eq(false);
 		expect(isValidPersonId('131052-308')).to.be.eq(false);
 	});
-	it('check gender', () => {
+	it('should check gender from hetu', () => {
 		expect(isMale('131052-308T')).to.be.eq(false);
 		expect(isFemale('131052-308T')).to.be.eq(true);
 	});
