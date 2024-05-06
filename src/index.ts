9cea3cc1dae08876377eeb774942912c8a123df3
Marko Harjula <marko.harjula@avanio.com>
2024-05-06 08:25:54 +0300

    change to tsup and dev updates

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,105 @@
 const c = '0123456789ABCDEFHJKLMNPRSTUVWXY';
 
-const getCC = (num: number): string | null => {
-	return c[num] ? c[num] : null;
-};
+/**
+ * Parse string to integer
+ * @throws TypeError if value is not a number
+ * @param value
+ * @returns value as number
+ */
+function toInt(value: string | undefined): number {
+	if (!value) {
+		throw new TypeError('undefined');
+	}
+	const outValue = parseInt(value, 10);
+	if (isNaN(outValue)) {
+		throw new TypeError(`${value} not a number`);
+	}
+	return outValue;
+}
 
-export const isValidPersonId = (hetu: string): boolean => {
-	if (hetu.length !== 11) {
+/**
+ * Validate that value is a string
+ * @throws TypeError if value is not a string
+ * @param value
+ * @returns value as string
+ */
+function getString(value: unknown): string {
+	if (typeof value !== 'string') {
+		throw new TypeError(`${JSON.stringify(value)} not a string`);
+	}
+	return value;
+}
+
+/**
+ * Build checksum for personId (last character)
+ * @param num - number to build checksum from
+ * @returns checksum character
+ */
+function buildCheckSum(num: number): string | undefined {
+	return c[num];
+}
+
+/**
+ * Construct array of string values from personId string
+ * @param personId - personId string
+ * @returns
+ */
+function buildCcValues(personId: string): [string, string, string, string, string, string, string, string, string, string, string] {
+	return [
+		getString(personId[0]),
+		getString(personId[1]),
+		getString(personId[2]),
+		getString(personId[3]),
+		getString(personId[4]),
+		getString(personId[5]),
+		getString(personId[6]),
+		getString(personId[7]),
+		getString(personId[8]),
+		getString(personId[9]),
+		getString(personId[10]),
+	];
+}
+
+/**
+ * Validate if string is a valid personId
+ * @param personId - personId string
+ * @returns true if valid, false if not
+ * @example
+ * isValidPersonId('131052-308T') // true
+ * isValidPersonId('131052-3082') // false
+ */
+export function isValidPersonId(personId: string): boolean {
+	if (personId.length !== 11) {
 		return false;
 	}
-	const d = hetu.toUpperCase();
-	return d[10] === getCC(parseInt(d[0] + d[1] + d[2] + d[3] + d[4] + d[5] + d[7] + d[8] + d[9], 10) % 31);
-};
+	const d = buildCcValues(personId.toUpperCase());
+	return d[10] === buildCheckSum(toInt(d[0] + d[1] + d[2] + d[3] + d[4] + d[5] + d[7] + d[8] + d[9]) % 31);
+}
 
-export const isMale = (hetu: string): boolean => {
-	if (!isValidPersonId(hetu)) {
-		throw new Error('not valid person id');
+/**
+ * Check if personId is for male
+ * @throws TypeError if personId is not valid
+ * @param personId - personId string
+ * @returns true if valid, false if not
+ * @example
+ * isMale('131052-308T') // false
+ */
+export function isMale(personId: string): boolean {
+	if (!isValidPersonId(personId)) {
+		throw new TypeError('not valid person id');
 	}
-	return parseInt(hetu[7] + hetu[8] + hetu[9], 10) % 2 === 1 ? true : false;
-};
+	const d = buildCcValues(personId);
+	return toInt(d[7] + d[8] + d[9]) % 2 === 1;
+}
 
-export const isFemale = (hetu: string): boolean => {
-	return !isMale(hetu);
-};
+/**
+ * Check if personId is for female
+ * @throws TypeError if personId is not valid
+ * @param personId - personId string
+ * @returns true if valid, false if not
+ * @example
+ * isFemale('131052-308T') // true
+ */
+export function isFemale(personId: string): boolean {
+	return !isMale(personId);
+}
--- test/testHetu.ts
+++ test/testHetu.ts
@@ -1,11 +1,14 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable import/first */
 process.env.NODE_ENV = 'test';
-import {expect} from 'chai';
 import 'mocha';
 import {isFemale, isMale, isValidPersonId} from '../src/';
+import {expect} from 'chai';
 
 describe('test hetu', () => {
 	it('should validate hetu', () => {
 		expect(isValidPersonId('131052-308T')).to.be.eq(true);
+		expect(isValidPersonId('131052A308T')).to.be.eq(true);
 		expect(isValidPersonId('131052-3082')).to.be.eq(false);
 	});
 	it('should fail if too long or short hetu', () => {
@@ -15,7 +18,7 @@ describe('test hetu', () => {
 	it('should check gender from hetu', () => {
 		expect(isMale('131052-308T')).to.be.eq(false);
 		expect(isFemale('131052-308T')).to.be.eq(true);
-		expect(isMale.bind(null,'')).to.throw('not valid person id');
-		expect(isFemale.bind(null,'')).to.throw('not valid person id');
+		expect(isMale.bind(null, '')).to.throw('not valid person id');
+		expect(isFemale.bind(null, '')).to.throw('not valid person id');
 	});
 });
